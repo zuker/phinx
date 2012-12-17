1be9f48989007cbe42b5ed3aefdbeb9013b97f54
Rob Morgan <robbym@gmail.com>
2012-12-17 14:06:59 +1100

    working on the phing task

--- lib/Phing/PhinxTask.php
+++ lib/Phing/PhinxTask.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'phing/Task.php';
-require_once 'phing/tasks/ext/phpcpd/PHPCPDFormatterElement.php';
+/*require_once 'phing/tasks/ext/phpcpd/PHPCPDFormatterElement.php';*/
 
 /**
  * Runs Phinx DB Migrations.
@@ -29,16 +29,18 @@ class PhinxTask extends Task
     /**
      * The Phinx command to run.
      *
+     * Defaults to 'migrate'.
+     *
      * @var string
      */
-    protected $_command;
+    protected $_command = 'migrate';
 
     /**
      * The target version to migrate to.
      *
      * @var float
      */
-    protected $_target;
+    protected $_target = 0;
 
     /**
      * Set the path to the Phinx configuration file.
@@ -62,6 +64,71 @@ class PhinxTask extends Task
     }
 
     /**
+     * Sets the specified environment to run Phinx migrations.
+     *
+     * @param string $environment The specified environment
+     * @return void
+     */
+    public function setEnvironment($environment)
+    {
+        $this->_environment = $environment;
+    }
+
+    /**
+     * Gets the specified environment to run Phinx migrations.
+     *
+     * @return string
+     */
+    public function getEnvironment()
+    {
+        return $this->_environment;
+    }
+
+    /**
+     * Sets the specified Phinx command to execute.
+     *
+     * Can either be 'migrate' or 'rollback'.
+     *
+     * @param string $command Command to execute.
+     * @return void
+     */
+    public function setCommand($command)
+    {
+        $this->_command = $command;
+    }
+
+    /**
+     * Gets the specified Phinx command to execute.
+     *
+     * @return string
+     */
+    public function getCommand()
+    {
+        return $this->_command;
+    }
+
+    /**
+     * Sets the target version to migrate to.
+     *
+     * @param float $target
+     * @return void
+     */
+    public function setTarget($target)
+    {
+        $this->_target = $target;
+    }
+
+    /**
+     * Gets the target version to migrate to.
+     *
+     * @return float
+     */
+    public function getTarget()
+    {
+        return $this->_target;
+    }
+
+    /**
      * Executes Phinx.
      *
      * @throws BuildException - if the Phinx classes can't be loaded.
@@ -74,5 +141,9 @@ class PhinxTask extends Task
          */
 
         // TODO - implement
+        echo $this->getConfiguration() . PHP_EOL;
+        echo $this->getEnvironment() . PHP_EOL;
+        echo $this->getCommand() . PHP_EOL;
+        echo $this->getTarget() . PHP_EOL;
     }
 }
